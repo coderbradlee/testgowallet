c18e742e16a9088e1bdd969c4df4869f220a73c0
limingming <limingming@wident.cn>
2017-11-27 11:26:11 +0800

    hdwallet

--- gowallet.go
+++ gowallet.go
@@ -23,7 +23,10 @@ func main() {
 func GenerateWallets(number uint32) (error) {
 	var err error
 	{
-		ret,_:=wallet.CreateWalletByteRandAndPwd(nil,"123456")
+		ret,err:=wallet.CreateWalletByteRandAndPwd(nil,"123456")
+		if err!=nil{
+			fmt.Println(err)
+		}
 		fmt.Println(ret)
 	}
 
